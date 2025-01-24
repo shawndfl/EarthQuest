e1d5024231fabe56260402b40f278d23dc44e94c
shawn <none@localhost.com>
2025-01-24 17:06:21 -0500

    New build

---
+++ src/systems/TileManager.ts
@@ -0,0 +1,6 @@
+import { TileComponent } from '../components/TileComponent';
+
+export class TileManager {
+  /** tile components created from the model data */
+  protected _tiles: TileComponent[][][];
+}
---
+++ src/testSupport/MockEngine.ts
@@ -0,0 +1,10 @@
+
+export class MockEngine {
+    constructor() { }
+    get height(): number {
+        return 600;
+    }
+    get width(): number {
+        return 800;
+    }
+}
